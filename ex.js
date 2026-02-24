758eca85353c0dc5c17431fb844799f1357af773
jobayerm10 <jobayermahmud976@gmail.com>
2026-02-24 13:47:32 +0600

    update files before pull

--- ex.js
+++ ex.js
@@ -1,14 +1,22 @@
-const user = {
-    name:'Jobayer',
-    address : {
-        city:'Rangpur'
-    }
+// const user = {
+    name: 'Jobayer',
+    address: {
+        city: 'Rangpur',
+        coordinates: {
+            lat: 25.7480,
+            lng: 89.2841
+        }
+    },
+    hobbies: ['coding', 'reading', 'gaming']
+};
 
-}
+// Shallow copy (changes to nested objects affect original)
+const shallowCopy = {...user};
+shallowCopy.address.city = 'Dhaka';
+console.log('Shallow copy - Original city:', user.address.city); // Dhaka (changed!)
 
-const newUser = {...user} // shallow copy
+// Deep copy using JSON method
+const deepCopy = JSON.parse(JSON.stringify(user));
+deepCopy.address.city = 'Chittagong';
+console.log('Deep copy - Original city:', user.address.city); // Dhaka (unchanged)
 
-
-newUser.address.city = 'mahmud'
-
-console.log(user.address.city)
